e08ffeb659dbb3f65c7b9657b697b1f1827c386d
uncommon-lamb <common.lamb@gmail.com>
2025-12-07 19:40:20 -0600

    first guix install

--- create-image.sh
+++ create-image.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
-# create location for sandbox
-## local
-here=$(realpath $(pwd))
-mach="${here}/machine"
-sandbox="${mach}"
-mkdir -p "${sandbox}"
-
-# pick location for temp
-#user=$(whoami)
-## one of
-#temp="${here}/temp" # fails if network drive
-#temp="/tmp/${user}/machine" # fails on &&&
-#temp="/var/tmp/${user}/machine" # fails on &&&
-#temp="/gpfs/fs7/aafc/scratch/sth003/machine" #fails on &&&
-#temp="/fs/vnas_Haafc/agr/sth003/machine"
-
-# set temp dir env variable. Cannot be a network drive&&&
-#TMPDIR="${temp}"
-#mkdir -p "${TMPDIR}" 
-
 echo "create overlay"
 rm -f ol.img
 # 1024:1G 10240:10G
-apptainer overlay create --fakeroot --size 10240 ol.img
+apptainer overlay create --fakeroot --size 1024 ol.img
 # &&& add to os.sif?
 
 echo "run installer I for overlay"
-apptainer exec  --fakeroot --overlay ol.img os.sif bash ./install-to-overlay_I.sh
+#apptainer exec  --fakeroot --overlay ol.img os.sif bash ./install-to-overlay_i.sh
+cat install-to-overlay_I.sh | apptainer exec --fakeroot --containall --no-home --overlay ol.img os.sif bash 
+
 # --contain --bind $(pwd)
 
 #echo "run installer II"
--- install-to-overlay_I.sh
+++ install-to-overlay_I.sh
@@ -11,31 +11,61 @@ echo "=== Container Environment Setup ==="
 whoami
 
 # SYSTEM UPDATE
-echo "" 
 echo "--- Update/Upgrade package lists ---" 
 
-apt update 
-apt upgrade 
+# &&& worked
+#apt update 
+#apt upgrade 
 echo "✓ Updated"
 
-## INSTALL GUIX
-#apk add guix
+# setup GUIX
 
-## create a group and user for the daemon to use
-#addgroup -S guix-daemon
+# create a group and user for the daemon to use (already done in pulled image)
+# groupadd --system guix-daemon
+#useradd -g guix-daemon -G guix-daemon               \
+          #-d /var/empty -s $(which nologin)          \
+          #-c "Guix daemon privilege separation user" \
+          #--system guix-daemon
+	  #
 
-#adduser \
-  #-h /var/empty \
-  #-g "Guix daemon privilege separation user" \
-  #-s $(which nologin) \
-  #-S \
-  #-G guix-daemon \
-  #guix-daemon
+# worked to show fail
+echo $(guix install hello)
 
-## start the daemon with its group
-#guix-daemon --build-users-group=guixbuild & # &&& make proper service with: apk add openrc
-## update, fails on containterizec ubuntu and alpine
+# worked
+guix describe 
+
+LC_ALL=C.UTF-8 
+
+#start the daemon using its group
+# supposed to be here
+/root/.config/guix/current/bin/guix-daemon --build-users-group=guixbuild &
+
+# browsed to find here in image
+#/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon # worked as root
+#/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild & # worked
+
+guix install --verbosity=0 hello
+echo "early exit"
+exit 0
+
+GUIX_PROFILE=/root/.config/guix/current;
+. "$GUIX_PROFILE/etc/profile"
+
+guix describe
+#=>Generation 2	Nov 28 2025 10:14:11	(current)  guix c9eb69d    repository URL: https://gitlab.com/debdistutils/guix/mirror.git    branch: master    commit: c9eb69ddbf05e77300b59f49f4bb5aa50cae0892
+
+guix install --verbosity=0 hello
+#=> accepted connection from pid 55, user rootThe following package will be installed:   hello 2.12.2
+
+GUIX_PROFILE="/root/.guix-profile"
+. "$GUIX_PROFILE/etc/profile"
+
+hello
+#=>Hello, world!
+
 #guix pull
+
+echo "✓ Guix pulled &&&"
 
 # we need to logout-login for the remaining guix install steps 
 # go to install II
@@ -48,6 +78,7 @@ echo "✓ Updated"
 #echo 'export PATH="/root/.config/guix/current/bin:$PATH"' >> /root/.bashrc
 #echo 'export GUIX_PROFILE="/root/.guix-profile"' >> /root/.bashrc
 #echo 'source "$GUIX_PROFILE/etc/profile"' >> /root/.bashrc
+
 ## Source for current session
 #export PATH="/root/.config/guix/current/bin:$PATH"
 #export GUIX_PROFILE="/root/.guix-profile"
