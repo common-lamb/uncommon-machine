f30fc30837e6714b17d5dc60652dfb1a2455c71b
uncommon-lamb <common.lamb@gmail.com>
2026-01-06 13:52:03 -0500

    derp

--- install-to-overlay_IIIII.sh
+++ install-to-overlay_IIIII.sh
@@ -1,66 +1,59 @@
 #!/bin/bash
 
-# Purpose: Install Lisp stack: stumpwm, Lish, lem, nyxt
+# Purpose: Install Lisp stack: kitty, Lish, lem, nyxt, xpra stumpwm
 
 set -e # Exit on error
 
 # apptainer shell does not source .profile or .bashrc
 source ~/.bashrc
 
-  # # INSTALL STUMPWM
-  # guix install stumpwm xpra cd ~/dotfiles stow stumpwm stow xpra #&&& remote setup 
 
-  #expra&&&
-  # echo "✓ StumpWM"
+# LEM
+guix install lem 
+#&&& ENV setting
+git clone https://github.com/fukamachi/.lem ~/.lem 
+mkdir -p ~/common-lisp/lem-vi-sexp 
+git clone git@github.com:fukamachi/lem-vi-sexp ~/common-lisp/lem-vi-sexp
+# cd ~/dotfiles &&& stow lem
+echo "✓ Lem"
 
-  # # NYXT
-  # guix install nyxt nyxt-emacs cd ~/dotfiles stow nyxt echo "✓ Nyxt"
 
-  # # LISH https://codeberg.org/nibbula/yew/src/branch/master/lish/docs/lish-examples.md over work vpn? does not like windows remote access
-  # temp="~/temp-lish" mkdir -p ${temp} && cd ${temp} cd git clone https://codeberg.org/nibbula/yew.git cd yew/lish
+# NYXT
+guix install nyxt nyxt-emacs 
+cd ~/dotfiles 
+stow nyxt 
+echo "✓ Nyxt"
 
-  # # modify vars.lisp search:path-append (defun hide-default-lishrc () (nos:path-append (nos:user-home) ".lishrc")) (defun default-lishrc () 
-  # # (path-append (namestring (user-homedir-pathname)) ".lishrc"))
 
-  # sh ./build.sh mv lish ~/.local/bin cd ~/dotfiles stow lish echo "✓ Lish"
 
-  # # LEM
-  # guix install lem git clone https://github.com/fukamachi/.lem ~/.lem mkdir -p ~/common-lisp && cd ~/common-lisp git clone 
-  # git@github.com:fukamachi/lem-vi-sexp
-  # # cd ~/dotfiles &&& stow lem
-  # echo "✓ Lem"
+# INSTALL STUMPWM
+guix install stumpwm xpra 
+cd ~/dotfiles 
+stow stumpwm 
+#&&& remote setup 
+cd ~/dotfiles 
+stow xpra 
+echo "✓ StumpWM"
 
-  # # EMACS
-  # guix install emacs &&& spacemacs &&& dotfiles
+# LISH 
+guix install kitty
+cd ~/dotfiles 
+stow kitty 
 
-#######################################
+temp="~/temp-lish"
+mkdir -p ${temp} && cd ${temp} 
+git clone https://github.com/common-lamb/yew.git
+cd yew/lish
 
-  # &&& conda gwl
+# may need to modify vars.lisp 
+# search:path-append 
+# (defun default-lishrc () (nos:path-append (nos:user-home) ".lishrc")) 
+# ->
+# (defun default-lishrc () (path-append (namestring (user-homedir-pathname)) ".lishrc"))
 
-  # # INSTALL BASE DEPENDENCIES
-  # echo "" echo "--- Installing base dependencies ---" apt-get install -y \
-  #         wget \ curl \ git \ build-essential \ ca-certificates \ gnupg \ xz-utils
-
-  # # INSTALL DEPENDENCIES
-  # echo "" echo "--- Installing dependencies ---" apt-get install -y \
-  #         expra \ emacs \
-
-  # echo "✓ installed"
-
-  # &&& expra
-  # 
-  # ################################################################################
-  # # Create user password &&&
-  # ################################################################################
-
-  # echo "" echo "--- Cleaning up ---"
-
-  # apt-get clean rm -rf /var/lib/apt/lists/*
-  # ################################################################################
-  # # SUMMARY
-  # ################################################################################
-
-  # echo "" echo "=== Setup Complete ===" echo "" echo "Installed software:" echo " - GNU Guix: $(guix --version | head -n1 || echo 'Check 
-  # PATH')" echo " - SBCL: $(sbcl --version)" echo " - StumpWM: $(which stumpwm)" echo " - Emacs: $(emacs --version | head -n1)" echo " - Xpra: 
-  # $(xpra --version | head -n1)"
-
+sh ./build.sh 
+mv lish ~/.local/bin 
+#cd ~/dotfiles stow lish 
+# https://codeberg.org/nibbula/yew/src/branch/master/lish/docs/lish-examples.md 
+# over work vpn? does not like windows remote access
+echo "✓ Lish"
